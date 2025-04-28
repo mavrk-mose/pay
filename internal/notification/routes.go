347734a4f9b07e7282d2b1a27ca62896c4639483
Moses Kippe <53339961+mavrk-mose@users.noreply.github.com>
2025-04-28 21:38:03 +0300

    Update routes.go

--- internal/notification/routes.go
+++ internal/notification/routes.go
@@ -5,6 +5,7 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/mavrk-mose/pay/pkg/middleware"
 	"github.com/mavrk-mose/pay/config"
+	"github.com/mavrk-mose/pay/pkg/utils"
 
 	handler "github.com/mavrk-mose/pay/internal/notification/handler"
 	userRepo "github.com/mavrk-mose/pay/internal/user/repository"
@@ -15,7 +16,9 @@ func NewApiHandler(r *gin.Engine, db *sqlx.DB, cfg *config.Config) {
 	userRepository := userRepo.NewUserRepository(db)                
 	notificationRepository := notificationRepo.NewNotificationRepo(db) 
 
-	notificationHandler := handler.NewNotificationHandler(r, cfg, userRepository, notificationRepository)
+	logger := utils.Logger()
+
+	notificationHandler := handler.NewNotificationHandler(r, cfg, userRepository, notificationRepository, logger)
 
 	// Notification Routes
 	api := r.Group("/api/v1", middleware.AuthMiddleware())
